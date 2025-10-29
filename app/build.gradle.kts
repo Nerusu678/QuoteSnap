8874f37ff46ae1aefab5475f9dc3351ad993acf4
Nerusu Naga Veera Venkata Satya Sivaram <sivaramnerusu8996@gmail.com>
2025-10-29 12:00:00 +0530

    Adding dependency for room, hilt, firebase auth, firestore, retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -49,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +61,41 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    // Hilt ,room ,navigation
+    implementation("androidx.room:room-runtime:2.7.0-alpha05")
+    kapt("androidx.room:room-compiler:2.7.0-alpha05")
+    implementation("androidx.room:room-ktx:2.7.0-alpha05")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    kapt("com.google.dagger:hilt-compiler:2.57.1")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+
+
+
+    // Retrofit
+    implementation( "com.squareup.retrofit2:retrofit:2.11.0")
+
+//    Json to kotlin object convertor mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp (optional but recommended)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+
+    implementation("androidx.compose.material:material-icons-extended:1.6.1")
+
+
+    // for splash screen optional
+    // SplashScreen API
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
