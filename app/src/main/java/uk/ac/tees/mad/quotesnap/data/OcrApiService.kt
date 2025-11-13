d8e8304c96d0edd6fdbf438b23fef43b75cc4e6e
Nerusu Naga Veera Venkata Satya Sivaram <sivaramnerusu8996@gmail.com>
2025-11-13 12:00:00 +0530

    Implemented the apiservice and network module

---
+++ app/src/main/java/uk/ac/tees/mad/quotesnap/data/OcrApiService.kt
@@ -0,0 +1,31 @@
+package uk.ac.tees.mad.quotesnap.data
+
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Query
+import uk.ac.tees.mad.quotesnap.data.models.OcrResponse
+
+interface OcrApiService {
+
+
+    @Multipart  // uploading an image
+    @POST("parse/image")
+    suspend fun extractTextFromImage(
+        @Query("apikey") apikey:String,
+        @Query("language") language: String = "eng",
+        @Query("isOverlayRequired") isOverlayRequired: Boolean = false,
+        @Query("detectOrientation") detectOrientation: Boolean = true,
+        @Query("scale") scale: Boolean = true,
+        @Query("OCREngine") ocrEngine: Int = 2,
+        @Part file: MultipartBody.Part
+    ): OcrResponse
+
+
+    companion object{
+        const val BASE_URL="https://api.ocr.space/"
+
+        const val API_KEY="K86066551388957"
+    }
+}
---
+++ app/src/main/java/uk/ac/tees/mad/quotesnap/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package uk.ac.tees.mad.quotesnap.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import uk.ac.tees.mad.quotesnap.data.OcrApiService
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(OcrApiService.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOcrApiService(retrofit: Retrofit): OcrApiService{
+        return retrofit.create(OcrApiService::class.java)
+    }
+}
